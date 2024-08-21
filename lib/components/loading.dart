686fac97b7da9a295b4619225397f7285aec368b
fabio-c266 <fabiocaldas266@gmail.com>
2024-08-21 10:15:37 -0300

    refactor: loading animation

--- lib/components/loading.dart
+++ lib/components/loading.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class Loading extends StatelessWidget {
   const Loading({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: CircularProgressIndicator.adaptive());
+    return Center(
+        child: LoadingAnimationWidget.waveDots(size: 100, color: Colors.grey));
   }
 }
